15acd7e43970284a02b57268dde15bb0f1680fb7
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-15 16:51:30 -0400

    Updated cors.rb to accept example.com

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000'   #'example.com'
+    origins 'example.com'  #'localhost:8000'   
 
     resource '*',
       headers: :any,
